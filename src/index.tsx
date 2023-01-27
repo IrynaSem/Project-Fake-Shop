5e4d1dd4279fb80506c8a829682fca1adf0cb45a
Iryna Sem <irynasem@MacBook-Pro-Iryna.local>
2023-01-27 23:13:53 +0200

    react ellement vis jsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const h1 = React.createElement('h1', { id: 'title' }, 'Hello React')
+//стандартний варіант
+// const h1 = React.createElement('h1', { id: 'title' }, 'Hello React')
 
-const list = React.createElement(
-    'ul',
-    null,
-    React.createElement('li', null, 'list Item 1'),
-    React.createElement('li', null, 'list Item 2'),
-    React.createElement('li', null, 'list Item 3')
+// const list = React.createElement(
+//     'ul',
+//     null,
+//     React.createElement('li', null, 'list Item 1'),
+//     React.createElement('li', null, 'list Item 2'),
+//     React.createElement('li', null, 'list Item 3')
+// )
+
+//jsx
+const h1 = <h1 id="title">Hello React js</h1>
+
+const list = (
+    <ul>
+        <li>list Item 1</li>
+        <li>list Item 2</li>
+        <li>list Item 3</li>
+    </ul>
 )
 
 const content = (
