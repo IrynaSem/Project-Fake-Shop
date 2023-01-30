a1538237e997e0c602192f2b29821866d225159e
Iryna Sem <irynasem@MacBook-Pro-Iryna.local>
2023-01-31 01:25:44 +0200

    прописали параметри і їх типи-пропси

--- src/index.tsx
+++ src/index.tsx
@@ -1,159 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { isPropertySignature } from 'typescript'
 
-//стандартний варіант
-// const h1 = React.createElement('h1', { id: 'title' }, 'Hello React')
-
-// const list = React.createElement(
-//     'ul',
-//     null,
-//     React.createElement('li', null, 'list Item 1'),
-//     React.createElement('li', null, 'list Item 2'),
-//     React.createElement('li', null, 'list Item 3')
-// )
-
-// //jsx
-// const h1 = (
-//     <h1 id="title" className="hghgh">
-//         Hello React js
-//     </h1>
-// )
-
-// let a = 10
-// let b = 'hello test'
-// const list = (
-//     <ul>
-//         <li>list Item {a + 10 + 10}</li>
-//         <li>list Item {b}</li>
-//         <li>list Item 3</li>
-//     </ul>
-// )
-
-// const content = (
-//     <div>
-//         {h1}
-//         {list}
-//     </div>
-// )
-// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-// root.render(<React.StrictMode>{content}</React.StrictMode>)
-
-// Компоненти в реакт
-// function App() {
-//     return <h1>Hello App.js</h1>
-// }
-// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-// root.render(
-//     <React.StrictMode>
-//         {' '}
-//         <App />
-//     </React.StrictMode>
-// )
-
-// Приклад самостійна
-// function Title() {
-//     return <h1>Hello App.js</h1>
-// }
-
-// function Content() {
-//     return (
-//         <div>
-//             <p>
-//                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-//                 Nesciunt corporis aliquam id mollitia incidunt officia dolore.
-//                 Vel quos nisi, numquam in aliquam atque itaque ex eligendi
-//                 dolores provident, alias eum?
-//             </p>
-//             <p>
-//                 Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-//                 Placeat nobis quae fugit natus incidunt accusamus facere ab eos,
-//                 necessitatibus et officiis dolorem, veniam quisquam ratione,
-//                 vitae blanditiis neque accusantium mollitia!
-//             </p>
-//         </div>
-//     )
-// }
-// function App() {
-//     return (
-//         <div>
-//             <Title />
-//             <Content />
-//         </div>
-//     )
-// }
-// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-// root.render(
-//     <React.StrictMode>
-//         {' '}
-//         <App />
-//     </React.StrictMode>
-// )
-// Та ж самостіна через стрілкову функцію
-// const Title = () => {
-//     return <h1>Hello App.js</h1>
-// }
-// const Content = () => {
-//     return (
-//         <React.Fragment>
-//             <p>
-//                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-//                 Nesciunt corporis aliquam id mollitia incidunt officia dolore.
-//                 Vel quos nisi, numquam in aliquam atque itaque ex eligendi
-//                 dolores provident, alias eum?
-//             </p>
-//             <p>
-//                 Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-//                 Placeat nobis quae fugit natus incidunt accusamus facere ab eos,
-//                 necessitatibus et officiis dolorem, veniam quisquam ratione,
-//                 vitae blanditiis neque accusantium mollitia!
-//             </p>
-//         </React.Fragment>
-//     )
-// }
-// function App() {
-//     return (
-//         <>
-//             <Title />
-//             <Content />
-//         </>
-//     )
-// }
-// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-// root.render(
-//     <React.StrictMode>
-//         {' '}
-//         <App />
-//     </React.StrictMode>
-// )
-
-//реакт фрагмент
-const Title = () => {
-    return <h1>Hello App.js</h1>
+interface ITitleProps {
+    text: string
 }
-const Content = () => {
+
+const Title = (props: ITitleProps) => {
+    console.log(props)
+    return <h1>Hello{props.text}</h1>
+}
+
+interface IContentProps {
+    text1: string
+    text2: string
+    year: number
+}
+const Content = (props: IContentProps) => {
     return (
-        <div>
-            <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Nesciunt corporis aliquam id mollitia incidunt officia dolore.
-                Vel quos nisi, numquam in aliquam atque itaque ex eligendi
-                dolores provident, alias eum?
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Placeat nobis quae fugit natus incidunt accusamus facere ab eos,
-                necessitatibus et officiis dolorem, veniam quisquam ratione,
-                vitae blanditiis neque accusantium mollitia!
-            </p>
-        </div>
+        <React.Fragment>
+            <p>{props.text1}</p>
+            <p>{props.text2}</p>
+            <div>Year:{props.year}</div>
+        </React.Fragment>
     )
 }
+
 function App() {
     return (
-        <div>
-            <Title />
-            <Content />
-        </div>
+        <>
+            <Title text="React" />
+            <Title text="TS" />
+            <Content text1="Hello world 1" text2="Hello world 2" year={2023} />
+        </>
     )
 }
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
