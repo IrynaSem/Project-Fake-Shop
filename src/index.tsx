a842674e9a9a635f4373c98c49a6709e9b0ad338
Iryna Sem <irynasem@MacBook-Pro-Iryna.local>
2023-01-31 11:28:00 +0200

    іменований експорт

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { isPropertySignature } from 'typescript'
-import Content from './Content'
+import { Content } from './Content'
 
 interface ITitleProps {
     text: string
