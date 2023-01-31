6ea89242e1402b21d5d944d72475879d34501f5d
Iryna Sem <irynasem@MacBook-Pro-Iryna.local>
2023-01-31 17:16:46 +0200

    створення і підключення scss

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,16 +1,15 @@
-import classes from './Content.module.css'
+import './Content.scss'
 
 type Props = {
     text1: string
     text2: string
     year: number
 }
-console.log(classes)
 
 export const Content = (props: Props) => {
     return (
         <>
-            <p className={`${classes.text}`}>{props.text1}</p>
+            <p className="text">{props.text1}</p>
             <p>{props.text2}</p>
             <div>Year:{props.year}</div>
         </>
