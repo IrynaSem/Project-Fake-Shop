8338ce47fd7c99353a4d2a6b6cc43a2d5e7e1d7c
Iryna Sem <irynasem@MacBook-Pro-Iryna.local>
2023-02-01 12:18:17 +0200

    створили лого

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -2,12 +2,12 @@ import * as React from 'react'
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
 import { Container } from '@mui/material'
 import './Header.scss'
+import Logo from 'components/Logo/Logo'
 type Props = {}
 const Header = (props: Props) => {
     return (
@@ -23,13 +23,7 @@ const Header = (props: Props) => {
                     >
                         <MenuIcon />
                     </IconButton>
-                    <Typography
-                        variant="h6"
-                        component="div"
-                        sx={{ flexGrow: 1 }}
-                    >
-                        News
-                    </Typography>
+                    <Logo />
                     <Button color="inherit">Login</Button>
                 </Toolbar>
             </Container>
