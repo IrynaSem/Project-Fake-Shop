45d06a57fa56449343ccc9bd3b3c1c4eb8a09207
Iryna Sem <irynasem@MacBook-Pro-Iryna.local>
2023-02-06 10:31:22 +0200

    add ProductsListItem

--- src/container/Main/Main.tsx
+++ src/container/Main/Main.tsx
@@ -1,5 +1,16 @@
+import { Container } from '@mui/system'
+import Home from 'pages/Home/Home'
+
 type Props = {}
 const main = (props: Props) => {
-    return <div>main</div>
+    return (
+        <Container
+            sx={{
+                padding: '60px 0',
+            }}
+        >
+            <Home />
+        </Container>
+    )
 }
 export default main
