ac36ab4bf1a1930b457ea1b00275a379d0c6ce44
Iryna Sem <irynasem@MacBook-Pro-Iryna.local>
2023-02-06 11:15:16 +0200

    write props

--- src/components/ProductsList/ProductsList.tsx
+++ src/components/ProductsList/ProductsList.tsx
@@ -11,15 +11,33 @@ const ProductsList = (props: Props) => {
             <Grid container spacing={3}>
                 <Grid item xs={12} sm={6} md={4}>
                     {' '}
-                    <ProductsListItem />
+                    <ProductsListItem
+                        title="iPhone 14 Pro"
+                        description="This is iPhone 14 pro"
+                        type=" phone"
+                        capacity="Capacity:256 "
+                        price={1200}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
                     {' '}
-                    <ProductsListItem />
+                    <ProductsListItem
+                        title="iPhone 13 Pro"
+                        description="This is iPhone 13 pro"
+                        type=" phone"
+                        capacity="Capacity:128 "
+                        price={900}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
                     {' '}
-                    <ProductsListItem />
+                    <ProductsListItem
+                        title="iPhone 12 Pro"
+                        description="This is iPhone 12 pro"
+                        type=" phone"
+                        capacity="Capacity:64 "
+                        price={500}
+                    />
                 </Grid>
             </Grid>
         </>
