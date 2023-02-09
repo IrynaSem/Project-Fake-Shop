7812c2d7648b861ba61c2115ce5823281f50efb0
Iryna Sem <irynasem@MacBook-Pro-Iryna.local>
2023-02-09 11:49:22 +0200

    add key i

--- src/components/ProductsList/ProductsList.tsx
+++ src/components/ProductsList/ProductsList.tsx
@@ -1,13 +1,7 @@
 import { Grid, Typography } from '@mui/material'
 import ProductsListItem from './ProductsListItem'
 import productsArray from 'pages/utils/productsArray'
-type ProductProps = {
-    title: string
-    description: string
-    type: string
-    capacity: string
-    price: number
-}
+
 type Props = {}
 const ProductsList = (props: Props) => {
     return (
@@ -18,14 +12,8 @@ const ProductsList = (props: Props) => {
 
             <Grid container spacing={3}>
                 {productsArray.map(
-                    ({
-                        title,
-                        description,
-                        type,
-                        capacity,
-                        price,
-                    }: ProductProps) => (
-                        <Grid item xs={12} sm={6} md={4}>
+                    ({ title, description, type, capacity, price }, i) => (
+                        <Grid item xs={12} sm={6} md={4} key={i}>
                             {' '}
                             <ProductsListItem
                                 title={title}
