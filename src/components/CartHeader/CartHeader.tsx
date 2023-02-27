1dff7f56dae35b50448a2033002b35e5423f0b28
Iryna Sem <irynasem@MacBook-Pro-Iryna.local>
2023-02-27 11:23:59 +0200

    add React and connect to app

--- src/components/CartHeader/CartHeader.tsx
+++ src/components/CartHeader/CartHeader.tsx
@@ -1,14 +1,27 @@
+import productsArray, {
+    getProductsObject,
+    Product,
+} from 'pages/utils/productsArray'
+
 type Props = {
     ProductsInCart: {
         [id: number]: number
     }
+    productsObject?: {
+        [id: number]: Product
+    }
 }
-const CartHeader = ({ ProductsInCart }: Props) => {
+const CartHeader = ({
+    ProductsInCart,
+    productsObject = getProductsObject(productsArray),
+}: Props) => {
+    console.log(productsObject)
     return (
         <div>
             {Object.keys(ProductsInCart).map((productId) => (
-                <div>
-                    {productId} :{ProductsInCart[parseInt(productId)]}
+                <div key={productId}>
+                    {productsObject[parseInt(productId)].title} : {''}
+                    {ProductsInCart[parseInt(productId)]}
                 </div>
             ))}
         </div>
